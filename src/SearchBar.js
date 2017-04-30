3239f207dfe99faa73427ca2733307eb43e7dd51
Matthew-Burfield <m1.burfield@gmail.com>
2017-04-30 13:58:00 +1000

    converted SearchBar back to a stateless functional component

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -7,17 +7,12 @@ import PropTypes from 'prop-types'
 import Form from './Form'
 import preload from '../public/data.json'
 
-// const SearchBar = ({ searchTerm, searchesCompleted, dispatch }, context) => {
-class SearchBar extends React.Component {
-  constructor (props, context) {
-    super(props)
-    this.handleSearchTermChange = this.handleSearchTermChange.bind(this)
-  }
-  handleSearchTermChange (e) {
-    // const searchChar = e.target.value.length > 0 ? e.target.value[0].toUpperCase() : ''
+const SearchBar = ({ searchTerm, searchesCompleted, dispatch }, context) => {
+  const handleSearchTermChange = (e) => {
+    const searchChar = e.target.value.length > 0 ? e.target.value[0].toUpperCase() : ''
     this.props.dispatch(setSearchTerm(e.target.value))
     this.props.dispatch(saveMarvelCharacters('A', preload.data.results))
-    // if (searchChar.length === 1 && !searchesCompleted.includes(searchChar)) {
+    if (searchChar.length === 1 && !searchesCompleted.includes(searchChar)) {
     //   const url = 'https://gateway.marvel.com:443/v1/public/'
     //   const apiKey = '8af0ed60c8e890096e71cace5997cea0'
     //   const hash = '54ea9c162ff7f33d4d418a0ea4629829'
@@ -33,33 +28,27 @@ class SearchBar extends React.Component {
     //     .catch(error => {
     //       console.error('axios error', error)
     //     })
-    // }
+    }
   }
 
-  render () {
-    return (
-      <div>
-        <img width='50%' height='auto' src='public/images/marvel-logo.png' alt='Marvel Logo' />
-        <h3 className='main-sub-title'>Your portal into the entire Marvel Universe!</h3>
-        <div className='search-container'>
-          <Form to='/search'>
-            <input
-              className='search-bar'
-              type='text'
-              placeholder='Search for your favorite MARVEL character'
-              value={this.props.searchTerm}
-              onChange={this.handleSearchTermChange}
-            />
-            <Link className='search-button' to='/search'><div className='search-icon' /></Link>
-          </Form>
-        </div>
+  return (
+    <div>
+      <img width='50%' height='auto' src='public/images/marvel-logo.png' alt='Marvel Logo' />
+      <h3 className='main-sub-title'>Your portal into the entire Marvel Universe!</h3>
+      <div className='search-container'>
+        <Form to='/search'>
+          <input
+            className='search-bar'
+            type='text'
+            placeholder='Search for your favorite MARVEL character'
+            value={searchTerm}
+            onChange={handleSearchTermChange}
+          />
+          <Link className='search-button' to='/search'><div className='search-icon' /></Link>
+        </Form>
       </div>
-    )
-  }
-}
-
-SearchBar.contextTypes = {
-  router: PropTypes.object
+    </div>
+  )
 }
 
 SearchBar.propTypes = {
